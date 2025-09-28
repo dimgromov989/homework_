4a5faaec766bd4afbfb18995684ef3cbdc8c95c0
Артем Громов <my_email.ru>
2025-09-28 15:43:17 +0300

    with date

--- src/widjet.py
+++ src/widjet.py
@@ -23,6 +23,13 @@ def mask_account_card(user_input: str) -> str:
             return f"{result_for_words} {get_mask_card_number(result_for_digit)}"
 
 
-result_ = mask_account_card(user_input=input("Введите номер карты или счета: "))
-print(result_)
+def get_date(user_input_data: str) -> str:
+    """Функция, реализующая вывод даты в нужном формате"""
+    return f"{user_input_data[8:10]}.{user_input_data[5:7]}.{user_input_data[:4]}"
 
+
+result_ = mask_account_card(user_input=input("Введите номер карты или счета: "))
+result_for_data = get_date(user_input_data=input("Введите дату: "))
+
+print(result_)
+print(result_for_data)
