60d5792461afe71a641791374a1c03941f46920e
Артем Громов <my_email.ru>
2025-11-16 13:44:51 +0300

    api_fixed_with_float

--- src/external_api.py
+++ src/external_api.py
@@ -4,14 +4,14 @@ import os
 from dotenv import load_dotenv
 
 
-def currency_conversion(transaction: dict):
+def currency_conversion(transaction: dict) -> float:
     """Функция, которая принимает на вход транзакцию и возвращает сумму транзакции (amount)
      в рублях, тип данных — float"""
     load_dotenv()
     if 'operationAmount' in transaction:
         if transaction.get('operationAmount').get('currency').get('code') == 'RUB':
             amount_for_rub = transaction.get('operationAmount').get('amount')
-            return f'Сумма транзакции: {amount_for_rub}'
+            return float(amount_for_rub)
         elif transaction.get('operationAmount').get('currency').get('code') in ['USD', 'EUR']:
             url = "https://api.apilayer.com/exchangerates_data/convert"
             payload = {
@@ -25,15 +25,14 @@ def currency_conversion(transaction: dict):
             }
             response = requests.get(url, headers=headers, params=payload)
             result_of_json = response.json()
-            result_rate = result_of_json['info']['rate']
             result_amount = result_of_json['result']
-            return f'Сумма транзакции в рублях: {result_amount}', f'Текущий курс: {result_rate}'
+            return result_amount
     else:
         return 'Нет суммы транзакции'
 
 
 result = currency_conversion(transaction={
-    "id": 41428825,
+    "id": 41428875,
     "state": "EXECUTED",
     "date": "2019-07-03T18:35:29.512364",
     "operationAmount": {
@@ -47,4 +46,5 @@ result = currency_conversion(transaction={
     "from": "MasterCard 7158300734726758",
     "to": "Счет 35383033474447895560"
 })
+
 print(result)
